27799686826626ef6c980a84fc4cc65c184a2189
julia1923 <juliaf.nascimento65@gmail.com>
2024-10-29 21:05:48 -0300

    Add files via upload

---
+++ fablea3/fablea3/src/main/java/br/com/fablea3/fablea3/Fablea3Application.java
@@ -0,0 +1,13 @@
+package br.com.fablea3.fablea3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Fablea3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Fablea3Application.class, args);
+	}
+
+}
---
+++ fablea3/fablea3/src/test/java/br/com/fablea3/fablea3/Fablea3ApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fablea3.fablea3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Fablea3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
