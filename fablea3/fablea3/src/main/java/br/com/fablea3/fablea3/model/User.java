f64eb70cbdb5b832c6572a55aa58eb84b0ccb223
Lucas Machado <110855649+lucasdoreal@users.noreply.github.com>
2024-10-29 21:05:48 -0300

    Add files via upload

---
+++ fablea3/fablea3/src/main/java/br/com/fablea3/fablea3/model/User.java
@@ -0,0 +1,14 @@
+import jakarta.persistence.Entity;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@Entity
+public class User {
+
+    private int id;
+    private String name;
+}
