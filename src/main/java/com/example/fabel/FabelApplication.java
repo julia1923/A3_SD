7126d5679adcc3c160075a2343022e22237463f1
MarcosVOS <marcos.oliveiraDEV@gmail.com>
2024-11-07 11:00:25 -0300

    chore: connect to db controller

--- src/main/java/com/example/fabel/FabelApplication.java
+++ src/main/java/com/example/fabel/FabelApplication.java
@@ -2,25 +2,10 @@ package com.example.fabel;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-@RestController
 public class FabelApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(FabelApplication.class, args);
-	}
-
-	@GetMapping("/")
-	public String rootRouter(){
-		return "Alive";
-	}
-
-	@GetMapping("/test_connection")
-	public String testConnection(){
-		System.out.println("Aqui3");
-		return "Alive";
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(FabelApplication.class, args);
+    }
 }
