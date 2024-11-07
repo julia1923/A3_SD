5e085ecaf9f83dd06e288df663bbf1cdbd1dc8b5
julia1923 <juliaf.nascimento65@gmail.com>
2024-11-07 00:01:42 -0300

    chore: add router `/` to alive

--- src/main/java/com/example/fabel/FabelApplication.java
+++ src/main/java/com/example/fabel/FabelApplication.java
@@ -2,12 +2,19 @@ package com.example.fabel;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class FabelApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(FabelApplication.class, args);
 	}
 
+	@GetMapping("/")
+	public String rootRouter(){
+		return "Alive";
+	}
 }
