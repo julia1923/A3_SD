853ce69f8384b852c480935d31eb35b6adb755c7
MarcosVOS <marcos.oliveiraDEV@gmail.com>
2024-11-06 23:05:21 -0300

    chore: unzip the folder

---
+++ fabel/src/test/java/com/example/fabel/FabelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.fabel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FabelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
