6fa5b72f68292f1959978524d0fe393df6890d58
julia1923 <juliaf.nascimento65@gmail.com>
2024-11-14 22:33:59 -0300

    created file users.java

---
+++ src/main/java/com/example/fabel/model/Users.java
@@ -0,0 +1,21 @@
+package com.example.fabel.model;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "Users")
+public class Users {
+    
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String username;
+    private String email;
+    private String password;
+    private String avatar;
+}
