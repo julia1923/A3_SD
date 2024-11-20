cc8cb3db8c95c686acdfed9bc43b3ebe988681c6
julia1923 <juliaf.nascimento65@gmail.com>
2024-11-20 19:33:51 +0000

    created security.java and spring security dependency

--- src/main/java/com/example/fabel/controller/UserController.java
+++ src/main/java/com/example/fabel/controller/UserController.java
@@ -1,9 +1,14 @@
 package com.example.fabel.controller;
 
+import java.io.IOException;
+import java.util.Base64;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -11,6 +16,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.fabel.repository.UserRepository;
@@ -23,6 +29,9 @@ public class UserController {
     @Autowired
     UserRepository userRepository;
 
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
     @GetMapping(value="/all")
     public List<Users> getAllUsers(){
         return userRepository.findAll();
@@ -34,11 +43,12 @@ public class UserController {
         Users createUser = new Users();
         createUser.setUsername(user.getUsername());
         createUser.setEmail(user.getEmail());
-        createUser.setPassword(user.getPassword());
+        createUser.setPassword(passwordEncoder.encode(user.getPassword()));
         createUser.setAvatar(user.getAvatar());
 
         return userRepository.save(createUser);
     }
+
 
     @PutMapping(value = "/update", produces = MediaType.APPLICATION_JSON_VALUE, consumes = MediaType.APPLICATION_JSON_VALUE)
     public Users updateUsers(@RequestBody Users user){
@@ -49,11 +59,32 @@ public class UserController {
         updateUser.setId(getUser.getId());
         updateUser.setUsername(user.getUsername());
         updateUser.setEmail(user.getEmail());
-        updateUser.setPassword(user.getPassword());
+        updateUser.setPassword(passwordEncoder.encode(user.getPassword()));
         updateUser.setAvatar(user.getAvatar());
 
         return userRepository.save(updateUser);
     }
+
+    @PutMapping(value = "/updateAvatar", consumes = { "multipart/form-data" })
+    public ResponseEntity<?> updateAvatar(@RequestParam("id") Long id,@RequestParam("avatar") String avatar) {
+
+    Users getUser = userRepository.findById(id).orElseThrow();
+
+    Users updateUser = new Users();
+
+    String avatarBase64;    
+
+    byte[] avatarBytes = avatar.getBytes();
+    avatarBase64 = Base64.getEncoder().encodeToString(avatarBytes); // Codificar para Base64
+     // Salvar a string Base64 no banco de dados
+     
+        updateUser.setId(getUser.getId());
+        updateUser.setAvatar(avatarBase64);
+
+        return ResponseEntity.status(HttpStatus.CREATED).body(userRepository.save(updateUser));
+    
+}
+
 
     @DeleteMapping(value="/delete/{id}",produces = MediaType.APPLICATION_JSON_VALUE)
     public Users deleteUser(@PathVariable Long id){
