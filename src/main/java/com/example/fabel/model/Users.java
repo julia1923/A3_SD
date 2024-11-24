dbbc49c8b4e145b41cddc5ea517363513c94457f
julia1923 <juliaf.nascimento65@gmail.com>
2024-11-24 00:31:53 +0000

    created tests

--- src/main/java/com/example/fabel/model/Users.java
+++ src/main/java/com/example/fabel/model/Users.java
@@ -12,7 +12,7 @@ public class Users {
     
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
     private String username;
     private String email;
