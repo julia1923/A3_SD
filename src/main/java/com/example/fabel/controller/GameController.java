80f14eaefbab58c57732749ea40ef67900a3d480
julia1923 <juliaf.nascimento65@gmail.com>
2024-11-25 23:39:25 +0000

    add images for games

--- src/main/java/com/example/fabel/controller/GameController.java
+++ src/main/java/com/example/fabel/controller/GameController.java
@@ -1,8 +1,12 @@
 package com.example.fabel.controller;
 
+import java.io.IOException;
+import java.util.Base64;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -10,7 +14,9 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.example.fabel.model.Games;
 import com.example.fabel.repository.GameRepository;
@@ -45,16 +51,11 @@ public class GameController {
 
         Games getGame = gameRepository.findById(game.getId()).orElseThrow();
 
-        System.out.println("\n\n\n\n\n\n\n\nsucesso2");
-        Games updateGame = new Games();
-        updateGame.setName(getGame.getName());
-        updateGame.setPrice(getGame.getPrice());
-        updateGame.setImage(getGame.getImage());
+        getGame.setName(game.getName());
+        getGame.setPrice(game.getPrice());
+        getGame.setImage(game.getImage());
 
-        System.out.println("\n\n\n\n\n\n\n\nsucesso3");
-
-        Games jogo = gameRepository.save(updateGame); 
-        System.out.println("qlqrcoisa");
+        Games jogo = gameRepository.save(getGame); 
 
         return jogo;
     }
@@ -63,7 +64,27 @@ public class GameController {
     public Games deleteGame(@PathVariable Long id){
         Games getGame = gameRepository.findById(id).orElseThrow();
         gameRepository.delete(getGame);
+        
+        System.out.println("\n\n\n\n\n\n\n\n\n\n" + getGame);
 
         return getGame;
+    }
+
+    @PutMapping(value = "/updateImage", consumes = { "multipart/form-data" })
+    public ResponseEntity<?> updateImage(@RequestParam("id") String idString, @RequestParam("avatar") MultipartFile avatar) throws IOException {
+
+        Long id = Long.parseLong(idString);
+        Games getGame = gameRepository.findById(id).orElseThrow();
+
+        String avatarBase64 = Base64.getEncoder().encodeToString(avatar.getBytes());
+
+        getGame.setImage(avatarBase64);
+    
+        if (avatar.getSize() > 5 * 1024 * 1024) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+            .body("Avatar file is too large. Maximum size is 5MB.");
+        }
+
+        return ResponseEntity.status(HttpStatus.CREATED).body(gameRepository.save(getGame));
     }
 }
